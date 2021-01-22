286a699af240b301889dbd13cbb07451483d5505
LatvianModder <latvianmodder@gmail.com>
2021-01-22 22:54:18 +0200

    Mod filter now only shows mods with items

--- common/src/main/java/dev/latvian/mods/itemfilters/item/ModFilterItem.java
+++ common/src/main/java/dev/latvian/mods/itemfilters/item/ModFilterItem.java
@@ -7,11 +7,16 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author LatvianModder
@@ -50,12 +55,21 @@ public class ModFilterItem extends StringValueFilterItem
 	{
 		List<StringValueFilterVariant> variants = new ArrayList<>();
 
-		for (Mod info : Platform.getMods())
+		LinkedHashSet<String> mods = new LinkedHashSet<>();
+
+		for (Map.Entry<ResourceKey<Item>, Item> entry : Registry.ITEM.entrySet())
 		{
-			StringValueFilterVariant variant = new StringValueFilterVariant(info.getModId());
-			variant.title = new TextComponent(info.getName());
-			variants.add(variant);
+			if (mods.add(entry.getKey().location().getNamespace()))
+			{
+				Mod info = Platform.getMod(entry.getKey().location().getNamespace());
+				StringValueFilterVariant variant = new StringValueFilterVariant(info.getModId());
+				variant.title = new TextComponent(info.getName());
+				variant.icon = new ItemStack(entry.getValue());
+				variants.add(variant);
+			}
 		}
+
+		variants.stream().filter(variant -> variant.id.equals("minecraft")).findFirst().ifPresent(variant -> variant.icon = new ItemStack(Items.GRASS_BLOCK));
 
 		return variants;
 	}
