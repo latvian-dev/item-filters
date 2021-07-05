907eabf4399cd231ddadcc18970523828fa8e07e
LatvianModder <latvianmodder@gmail.com>
2021-07-05 03:27:46 +0300

    More fixes

--- fabric/src/main/java/dev/latvian/mods/itemfilters/ItemFiltersFabric.java
+++ fabric/src/main/java/dev/latvian/mods/itemfilters/ItemFiltersFabric.java
@@ -1,7 +1,6 @@
 package dev.latvian.mods.itemfilters;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 
 /**
  * @author LatvianModder
@@ -9,12 +8,6 @@ import net.fabricmc.loader.api.FabricLoader;
 public class ItemFiltersFabric implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		try {
-			ItemFilters mod = new ItemFilters();
-			FabricLoader.getInstance().getEntrypoints("itemfilters-init", ItemFiltersFabricInitializer.class).forEach(ItemFiltersFabricInitializer::init);
-			mod.setup();
-		} catch (Throwable throwable) {
-			throw new RuntimeException(throwable);
-		}
+		new ItemFilters().setup();
 	}
 }
