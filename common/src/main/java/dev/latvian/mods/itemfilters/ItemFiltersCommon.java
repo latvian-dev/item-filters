2200e8db2ecd62e8998368ddb21c3bc350f602b3
Des Herriott <des.herriott@gmail.com>
2023-07-25 09:16:14 +0100

    fix: populate the creative search tab when necessary
    When searching for matching stacks, search tab is not necessarily filled yet

--- common/src/main/java/dev/latvian/mods/itemfilters/ItemFiltersCommon.java
+++ common/src/main/java/dev/latvian/mods/itemfilters/ItemFiltersCommon.java
@@ -1,8 +1,14 @@
 package dev.latvian.mods.itemfilters;
 
+import dev.architectury.utils.GameInstance;
 import dev.latvian.mods.itemfilters.api.IStringValueFilter;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.Optional;
 
 /**
  * @author LatvianModder
@@ -12,5 +18,15 @@ public class ItemFiltersCommon {
 	}
 
 	public void openStringValueFilterScreen(IStringValueFilter filter, ItemStack stack, InteractionHand hand) {
+	}
+
+	public Optional<RegistryAccess> registryAccess() {
+		if (GameInstance.getServer() != null) {
+			ServerLevel overworld = GameInstance.getServer().getLevel(Level.OVERWORLD);
+			if (overworld != null) {
+				return Optional.of(overworld.registryAccess());
+			}
+		}
+		return Optional.empty();
 	}
 }
