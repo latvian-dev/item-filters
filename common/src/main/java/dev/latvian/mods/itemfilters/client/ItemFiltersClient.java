9ca2a392c3b5dc72470fc27f6e0b3f7a2cf686f3
Max <maxh2709@gmail.com>
2023-08-29 18:11:18 +0200

    fix: populate the creative search tab when necessary (#40) (#42)
    Co-authored-by: desht <des.herriott@gmail.com>

--- common/src/main/java/dev/latvian/mods/itemfilters/client/ItemFiltersClient.java
+++ common/src/main/java/dev/latvian/mods/itemfilters/client/ItemFiltersClient.java
@@ -8,9 +8,12 @@ import dev.latvian.mods.itemfilters.gui.InventoryFilterMenu;
 import dev.latvian.mods.itemfilters.gui.InventoryFilterScreen;
 import dev.latvian.mods.itemfilters.gui.StringValueFilterScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.Optional;
 
 /**
  * @author LatvianModder
@@ -26,5 +29,10 @@ public class ItemFiltersClient extends ItemFiltersCommon {
 	@Override
 	public void openStringValueFilterScreen(IStringValueFilter filter, ItemStack stack, InteractionHand hand) {
 		Minecraft.getInstance().setScreen(new StringValueFilterScreen(filter, stack, hand));
+	}
+
+	@Override
+	public Optional<RegistryAccess> registryAccess() {
+		return Minecraft.getInstance().level == null ? Optional.empty() : Optional.of(Minecraft.getInstance().level.registryAccess());
 	}
 }
